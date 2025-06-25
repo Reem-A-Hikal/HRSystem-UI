f26f66744805fc762b612c280d2622a1fcfbf11d
dua74 <helaldua74@outlook.com>
2025-06-26 00:21:02 +0300

    Employees-data-component

---
+++ src/app/components/employeesdata/employeesdata.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeesdata',
+  imports: [],
+  templateUrl: './employeesdata.component.html',
+  styleUrl: './employeesdata.component.css'
+})
+export class EmployeesdataComponent {
+
+}
