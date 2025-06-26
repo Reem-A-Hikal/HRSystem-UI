61151689a4f7489a49fe2cb6a8df2d4c2407cf25
dua74 <helaldua74@outlook.com>
2025-06-26 18:15:52 +0300

    edit-employeesdata

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RoleManagementComponent } from './components/role-management/role-management.component';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { EmployeesdataComponent } from './components/employeesdata/employeesdata.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
-    children: [{ path: 'roles', component: RoleManagementComponent }],
+    children: [
+      { path: 'roles', component: RoleManagementComponent },
+      { path: 'employees', component: EmployeesdataComponent },
+    ],
   },
 ];
