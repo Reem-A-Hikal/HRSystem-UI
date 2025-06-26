91457942d2ad58e70d97a573c806467a7ee7336d
Reem-A-Hikal <reemhikal19@gmail.com>
2025-06-26 16:52:59 +0300

    Added Login Component

---
+++ src/app/services/Toastr.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ToastrService as NgxToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ToastrService {
+
+  constructor(private toastr: NgxToastrService) { }
+
+  onSuccess(message: string, title?: string) {
+    this.toastr.success(message, title, {
+      timeOut: 3000,
+      progressBar: true,
+      closeButton: true,
+      positionClass: 'toast-top-right',
+    });
+  }
+
+  onError(message: string, title?: string) {
+    this.toastr.error(message, title, {
+      timeOut: 3000,
+      progressBar: true,
+      closeButton: true,
+      positionClass: 'toast-top-right',
+    });
+  }
+}
