2d0c83f3d75cf3b6281b7fa114815146a3b7e9db
Reem-A-Hikal <reemhikal19@gmail.com>
2025-06-26 17:40:21 +0300

    Enhance Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { TopbarComponent } from './layout/topbar/topbar.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, TopbarComponent, RouterModule],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RoleManagementComponent } from './components/role-management/role-management.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'roles', component: RoleManagementComponent },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [{ path: 'roles', component: RoleManagementComponent }],
+  },
 ];
