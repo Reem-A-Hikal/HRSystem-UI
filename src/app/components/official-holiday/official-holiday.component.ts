0511b2980480a24ff09e71b80e1b73c2d5361346
MostafaMohsen58 <mostafa.elnahas5884@gmail.com>
2025-06-30 03:19:04 +0300

    Add holiday service

--- src/app/components/official-holiday/official-holiday.component.ts
+++ src/app/components/official-holiday/official-holiday.component.ts
@@ -2,6 +2,10 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component , OnInit} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HolidayService } from '../../services/holiday.service';
+import { IHoliday, IHolidayResponse } from '../../models/IHoliday';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-official-holiday',
@@ -11,38 +15,52 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./official-holiday.component.css']
 })
 export class OfficialHolidayComponent implements OnInit {
-holidays = [
-  {id:1,name: 'Dependence day', date: '2025-10-04'}, 
-  {id:2,name: 'Eid Al-Adha', date: '2025-6-1'}
-];
+// holidays = [
+//   {id:1,name: 'Dependence day', date: '2025-10-04'}, 
+//   {id:2,name: 'Eid Al-Adha', date: '2025-6-1'}
+// ];
+holidays: IHolidayResponse[] = [];
 
   holidayName = '';
   holidayDate = '';
 
-  // constructor(private http: HttpClient) {}
+ constructor(private service:HolidayService, private router:Router,private toastr:ToastrService) {}
 
   ngOnInit() {
     this.loadHolidays();
   }
 
   loadHolidays() {
-    // this.http.get<any[]>('/api/holidays').subscribe(data => this.holidays = data);
+    this.service.getHolidays().subscribe(data => this.holidays = data);
   }
 
   createHoliday() {
-    // const newHoliday = { name: this.holidayName, date: this.holidayDate };
-    // this.http.post('/api/holidays', newHoliday).subscribe(() => {
-    //   this.loadHolidays();
-    //   this.holidayName = '';
-    //   this.holidayDate = '';
-    // });
+    const newHoliday: IHoliday = {
+      name: this.holidayName,
+      date: this.holidayDate
+    };
+
+    this.service.createHoliday(newHoliday).subscribe(() => {
+      this.loadHolidays();
+      this.holidayName = '';
+      this.holidayDate = '';
+    });
   }
 
-  editHoliday(holiday: any) {
-    //logic
+  editHoliday(id: number) {
+    this.router.navigate([`/dashboard/edit-official-holiday/${id}`]);
   }
 
   deleteHoliday(id: number) {
-    // this.http.delete(`/api/holidays/${id}`).subscribe(() => this.loadHolidays());
+    this.service.deleteHoliday(id).subscribe(
+      (response) => {
+        this.toastr.success('Deleted successful', 'Success')
+        this.loadHolidays();
+      },
+      (error) => {
+        this.toastr.error('Error', 'Error');
+        console.log(error);
+      }
+    );
   }
 }
---
+++ src/app/models/IHoliday.ts
@@ -0,0 +1,10 @@
+export interface IHoliday {
+  name: string;
+  date:string;
+}
+
+export interface IHolidayResponse {
+  id: number;
+  name: string;
+  date: string;
+}
