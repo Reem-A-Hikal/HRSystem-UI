2c76dd620b9892edaedd7bb539a0eee4074fc5b5
MostafaMohsen58 <mostafa.elnahas5884@gmail.com>
2025-07-14 15:26:50 +0300

    Add setting service and validation

---
+++ src/app/services/Setting.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+
+export enum SettingType {
+  Hour = 1,
+  Pound = 2,
+}
+
+
+export interface EditSettingDto {
+  type: SettingType;
+  overTime: number;
+  deduction: number;
+  firstHoliday: string;
+  secondHoliday: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingService {
+
+  constructor(private http :HttpClient ) { }
+  getSettings(): Observable<EditSettingDto> {
+    return this.http.get<EditSettingDto>(`${environment.apiBaseUrl}/settings`);
+  }
+
+  updateSettings(dto: EditSettingDto): Observable<any> {
+    return this.http.put(`${environment.apiBaseUrl}/settings/update`, dto);
+  }
+}
